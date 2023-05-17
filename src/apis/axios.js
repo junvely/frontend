4f2d7f68e51f23cef3002d69d71587b4e27d2081
yooneeee <sjy4489@gmail.com>
2023-05-18 03:19:41 +0900

    Fix: 토큰 에러 수정

--- src/apis/axios.js
+++ src/apis/axios.js
@@ -26,11 +26,23 @@ instance.interceptors.response.use(
     const accessToken = response.data["accessToken"];
     const refreshToken = response.data["refreshToken"];
     const userId = response.data["userId"];
-    if (accessToken || refreshToken) {
+
+    if (accessToken) {
+      sessionStorage.setItem("accessToken", accessToken);
+    }
+    if (refreshToken) {
+      sessionStorage.setItem("refreshToken", refreshToken);
+    }
+    if (userId) {
+      sessionStorage.setItem("userId", userId);
+    }
+    /* if (accessToken && refreshToken) {
       sessionStorage.setItem("accessToken", accessToken);
       sessionStorage.setItem("refreshToken", refreshToken);
       sessionStorage.setItem("userId", userId);
-    }
+    } else {
+      sessionStorage.setItem("accessToken", accessToken);
+    } */
     return response;
   },
 
